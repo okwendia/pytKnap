6e02f89d8e4d3415a11af6e44a7ed221b82da973
o.karup <o.karup@gmail.com>
2025-05-17 19:42:29 +0200

    pwa

---
+++ service-worker.js
@@ -0,0 +1,16 @@
+self.addEventListener("install", e => {
+    e.waitUntil(
+      caches.open("pyt-cache").then(cache => {
+        return cache.addAll(["./", "./index.html", "./style.css", "./script.js"]);
+      })
+    );
+  });
+  
+  self.addEventListener("fetch", e => {
+    e.respondWith(
+      caches.match(e.request).then(response => {
+        return response || fetch(e.request);
+      })
+    );
+  });
+  
